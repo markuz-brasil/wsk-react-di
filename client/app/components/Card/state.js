6b8e4d5a52d984818872e6beb836753ce56b2ab4
Markuz GJ <markuz.br@gmail.com>
2014-09-27 02:37:27 -0700

    added mock DI

---
+++ client/app/components/Card/state.js
@@ -0,0 +1,37 @@
+import {Inject, Provide} from 'di'
+import {BaseState} from '../Core'
+
+export class Title {
+  toString () {
+    return 'title: '+ Math.random()
+  }
+}
+
+export class Body {
+  toString () {
+    return 'body: '+ Math.random()
+  }
+}
+
+@Provide(Body)
+export class MockBody {
+  toString () {
+    return 'mock:'
+  }
+}
+
+@Provide(Title)
+export class MockTitle {
+  toString () {
+    return 'mock:'
+  }
+}
+
+@Inject(Body, Title)
+export class CardState extends BaseState {
+  constructor(body, title) {
+    return super({ body: body, title: title })
+  }
+}
+
+
