e9e96d7813be4de3d4cd714d366303bec6b48c94
Markuz GJ <markuz.br@gmail.com>
2014-10-28 00:17:25 -0700

    forked co into the proj

---
+++ client/src/core/libs/index.js
@@ -0,0 +1,103 @@
+
+var {di, assert } = window.BundleNamespace.coreLibs
+import * as co from './co'
+
+export {co, di, assert}
+
+export function c0 (fn) {
+  return function c0 (...args) {
+    if (args.length === 0) return co(fn)()
+
+    var next = args.pop()
+
+    if (typeof next !== 'function') {
+      return co(fn)(next)
+    }
+
+    args.push((err, value) => {
+      if (err) { return console.error(err) }
+
+      try { next(value) }
+      catch (err) {
+        if (err) { return console.error(err) }
+      }
+
+    })
+
+    co(fn)(...args)
+  }
+}
+
+// di.InjectSync = di.Inject
+// di.ProvideSync = di.Provide
+// di.InjectorSync = di.Injector
+
+// class InjectLazySync extends di.InjectLazy {
+//   constructor(...args) {
+//     super(...args)
+//     this.async = false
+//     console.log('InjectLazySync', args)
+//   }
+// }
+
+// class InjectAsync extends di.Inject {
+//   constructor (...args) {
+//     console.log('InjectAsync', args)
+//     super(...args)
+//     this.async = true
+//   }
+// }
+
+// class InjectLazyAsync extends di.InjectLazy {
+//   constructor (...args) {
+//     super(...args)
+//     this.async = true
+//     console.log('InjectLazyAsync', args, this)
+
+//   }
+// }
+
+// class ProvideAsync extends di.Provide {
+//   constructor (...args) {
+//     super(...args)
+//     this.async = true
+//     console.log('ProvideAsync', args, this)
+
+//   }
+// }
+
+// class InjectorAsync extends di.Injector {
+//   constructor (...args) {
+//     this.store = this.store || new Map
+//     this.store.set(args)
+//     super(...args)
+//     this.async = true
+//     // console.log('InjectorAsync', args, this.get)
+
+//   }
+//   get (...args) {
+//     var instance = super(...args)
+//     if (!this.async) return instance
+//     // console.log(di.Injector.prototype.get)
+//     return instance
+//   }
+// }
+
+// di.InjectLazySync = InjectLazySync
+
+// di.InjectAsync = InjectAsync
+// di.InjectLazyAsync = InjectLazyAsync
+// di.ProvideAsync = ProvideAsync
+// di.InjectorAsync = InjectorAsync
+
+// export {
+//   Injector,
+//   annotate,
+//   Inject,
+//   InjectLazy,
+//   InjectPromise,
+//   Provide,
+//   ProvidePromise,
+//   SuperConstructor,
+//   TransientScope
+// } from 'di'
