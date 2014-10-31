031231fa454beebb0546c450940f83649180db64
Markuz GJ <markuz.br@gmail.com>
2014-10-31 15:31:59 -0700

    adding docs to flux arch

--- client/flux/annotations.js
+++ client/flux/annotations.js
@@ -9,27 +9,49 @@ var {
   TransientScope
 } = di
 
+// TODO: JSDOC this stuff
+
 //
 // TODO: explain vocabulary.
 // Things like Dispatcher, Actions, Providers, Services and Injectors
 // Also things like Action Iterators and Action Generator
 //
+// Explain the role of each Provider
+//
+
+//
+// A dispacher is nothing more than an instance of di's Injector
+// Maybe it could be a class that extends Injector
+//
+
 export function Dispatcher () { return new Injector([]) }
 
 //
 // Initialization Actions
 //
+
+//
+// The Context Action Iterator should return the Object which would be
+// the input of React.createClass.
+// And initiate nextTick once the whole context is ready
+//
+
 annotate(Context, new Inject(InitState, NextTick))
 export function Context (init, nextTick) {
   var iterator = Context()
-  function * Context () {}
+  function * Context () {return {render, getInitialState}}
   return iterator
 }
+
+//
+// The InitState Action Iterator should combine InitStore and
+// InitView and return an Object where the Context Action can pass it along
+//
 
 annotate(InitState, new Inject(InitStore, InitView))
 export function InitState (store, view) {
   var iterator = InitState()
-  function * InitState () {}
+  function * InitState () {return {render, getInitialState}}
   return iterator
 }
 
@@ -37,9 +59,14 @@ export function InitState (store, view) {
 // Views
 //
 
+//
+// This is a View Service. It is a "singleton"
+//
+
 var _view = {}
 export function View () { return _view }
 
+//
 annotate(InitView, new Inject(View))
 export function InitView (view) {
   var iterator = InitView()
